d55e7c8f090908215ba62eae4f2e61c033faf622
Harshanan Saddanathan <hsaddanathan21@gmail.com>
2020-12-15 19:47:06 -0500

    Edited updateQuestion.js file in order to complete our PUT route

--- public/assets/js/updateQuestion.js
+++ public/assets/js/updateQuestion.js
@@ -2,12 +2,19 @@ $(document).ready(function () {
   $("#updateQuestion").on("submit", function (e) {
     e.preventDefault();
     const id = $(this).data("id");
-    const question = $("#question").val();
-    const optionA = $("#optionA").val();
-    const optionB = $("#optionB").val();
-    const optionC = $("#optionC").val();
-    const optionD = $("#optionD").val();
-    const answer = $("#answer").val();
+    const question = $("#editQuestion").val();
+    const optionA = $("#updateOptionA").val();
+    const optionB = $("#updateOptionB").val();
+    const optionC = $("#updateOptionC").val();
+    const optionD = $("#updateOptionD").val();
+    const answer = $("#updateAnswer").val();
+    console.log(id);
+    console.log(question);
+    console.log(optionA); 
+    console.log(optionB);
+    console.log(optionC);
+    console.log(optionD);
+    console.log(answer);
 
     $.ajax({
       method: "PUT",
